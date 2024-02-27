c0ca7b1a457585c5dce5505e0383539c0924c76d
singsangssong <plmko0914@gmail.com>
2024-02-27 18:05:42 +0900

    Initial commit

---
+++ src/main/java/search/search_practice/SearchPracticeApplication.java
@@ -0,0 +1,13 @@
+package search.search_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/search/search_practice/SearchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package search.search_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
